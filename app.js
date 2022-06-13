55e86f508840b0d0d2cc6a82722de95bd141182d
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-06-13 05:06:08 -0700

    fxied width of mobile nav, and tidied js file

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const navBar = document.querySelector(".mobile-nav");
 
 function showNav() {
-    navBar.style.transition = "1s";
+
     navBar.style.right = "0px";
     navBar.style.display = "block";
-    navBar.style.transition = "all 2s";
 }
 
 function hideNav() {
-    navBar.style.transition = "1s";
     navBar.style.right = "-300px";
     navBar.style.display = "none";
 }
